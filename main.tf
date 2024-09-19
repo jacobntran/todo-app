0aa2aea5ecdc19bd4d47000b20c8760907ac6c78
Jacob Tran <jtran@way2b1.com>
2024-09-19 13:54:07 -0700

    added database resources

--- main.tf
+++ main.tf
@@ -11,8 +11,24 @@ variable "my_ip" {
   description = "Your ip address"
 }
 
+variable "rds_db_name" {
+  description = "Name of your RDS database"
+}
+
+variable "rds_user" {
+  description = "RDS User"
+}
+
+variable "rds_password" {
+  description = "Password for your RDS User"
+}
+
 output "app_node_public_ip" {
   value = aws_instance.app.public_ip
+}
+
+output "rds_endpoint" {
+    value = aws_db_instance.this.endpoint
 }
 
 ### NETWORKING ###
@@ -93,28 +109,28 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-# resource "aws_route_table" "db" {
-#   vpc_id = aws_vpc.private.id
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.private.id
 
-#   route {
-#     cidr_block                = aws_vpc.public.cidr_block
-#     vpc_peering_connection_id = aws_vpc_peering_connection.this.id
-#   }
+  route {
+    cidr_block                = aws_vpc.public.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.this.id
+  }
 
-#   tags = {
-#     Name = "Todo App Database Subnet Route Table"
-#   }
-# }
+  tags = {
+    Name = "Todo App Database Subnet Route Table"
+  }
+}
 
-# resource "aws_route_table_association" "db" {
-#   subnet_id      = aws_subnet.db.id
-#   route_table_id = aws_route_table.db.id
-# }
+resource "aws_route_table_association" "db" {
+  subnet_id      = aws_subnet.db.id
+  route_table_id = aws_route_table.db.id
+}
 
-# resource "aws_route_table_association" "db_2" {
-#   subnet_id      = aws_subnet.db_2.id
-#   route_table_id = aws_route_table.db.id
-# }
+resource "aws_route_table_association" "db_2" {
+  subnet_id      = aws_subnet.db_2.id
+  route_table_id = aws_route_table.db.id
+}
 
 resource "aws_vpc_peering_connection" "this" {
   peer_vpc_id = aws_vpc.private.id
@@ -159,9 +175,39 @@ resource "aws_security_group" "app" {
   }
 
   egress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = [aws_vpc.private.cidr_block]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+### DATA ###
+resource "aws_db_instance" "this" {
+  allocated_storage      = 20
+  db_name                = var.rds_db_name
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  username               = var.rds_user
+  password               = var.rds_password
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+}
+
+resource "aws_db_subnet_group" "this" {
+  name       = "todo_app_db_sn_group"
+  subnet_ids = [aws_subnet.db.id, aws_subnet.db_2.id]
+}
+
+resource "aws_security_group" "rds" {
+  name   = "rds_db_sg"
+  vpc_id = aws_vpc.private.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.public.cidr_block]
   }
 }
